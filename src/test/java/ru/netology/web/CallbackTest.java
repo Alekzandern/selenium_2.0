984ca0147815a3a75567b6ef8a55b16f6e6f30bb
Alekzandern <130380585+Alekzandern@users.noreply.github.com>
2023-08-29 15:27:11 +0000

    Update CallbackTest.java

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
