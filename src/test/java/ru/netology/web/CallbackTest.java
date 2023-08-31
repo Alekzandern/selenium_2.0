0d33bedbd2c2be1546dd92ce800b352d69f4da06
Alexzander <Alex.Zander54.ru>
2023-08-31 23:24:56 +0000

    ft commit

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -41,14 +41,13 @@ class CallbackTest {
     }
 
     @Test
-    void shouldTestV1() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Василий");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    public void shouldTestV1() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алик Сеев-Алексеев");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79123456789");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualtext = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualtext);
     }
 }
 
